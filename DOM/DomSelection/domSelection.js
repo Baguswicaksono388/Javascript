606ea401d7ccb4c725544c4871c32e8cb071dfd4
Bagus Wicaksono <baguswicaksono388@gmail.com>
2019-08-19 06:26:07 +0700

    [Dev] Merapikan file

---
+++ DOM/DomSelection/domSelection.js
@@ -0,0 +1,7 @@
+// DOM Selection
+
+const judul = document.getElementById('judul'); //.. variabel dan judul tidak harus sama. Kemudian jika kita sdh menangkap id tersebut maka kita akan bebas mengubahnya, misal stylenya pada id judul tersebut.
+//Manipulasi id judul yang tadi dibuat
+judul.style.color = 'red'; //.. Untuk mengubah Warna Color pada element dengan id Judul
+judul.style.backgroundColor = 'grey'; //.. Untuk mengubah Warna Background pada element dengan id Judul
+judul.innerHTML = 'Bagus';
--- DOM/domSelection.js
+++
@@ -1,3 +0,0 @@
-// DOM Selection
-
-const judul = document.getElementById('judul'); //.. variabel dan judul tidak harus sama. Kemudian jika kita sdh menangkap id tersebut maka kita akan bebas mengubahnya, misal stylenya pada id judul tersebut.
