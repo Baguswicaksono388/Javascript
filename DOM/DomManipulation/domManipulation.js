db89278518f6ea0b0f0cceb2ae95b570f112dd35
Bagus Wicaksono <baguswicaksono388@gmail.com>
2019-08-20 06:18:22 +0700

    [Dev] querySelector

--- DOM/DomManipulation/domManipulation.js
+++ DOM/DomManipulation/domManipulation.js
@@ -1,2 +1,11 @@
-// const judul = document.getElementById('judul');
-// judul.innerHTML = '<em>Bagus</em>';
+// // const judul = document.getElementById('judul');
+// // judul.innerHTML = '<em>Bagus</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello wordl'; //.. Pada sectionA ini yang kita traget sebenarna ada 3 paragraf dan 1 button, jika ini kita melakukan innerHtml maka 3 paragraf dan 1 button akan hilang dan digantikan Hello word.
+
+//element.style.<property>
+// const judul = document.querySelector('h1#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
