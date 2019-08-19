8ab631a242055c3289ed153148033d56f19a3347
Bagus Wicaksono <baguswicaksono388@gmail.com>
2019-08-20 06:30:48 +0700

    [Dev] setAttribute, getAttribute, removeAttribute

--- DOM/DomManipulation/domManipulation.js
+++ DOM/DomManipulation/domManipulation.js
@@ -9,3 +9,10 @@
 // judul.style.color = 'lightblue';
 // judul.style.backgroundColor = 'salmon';
 
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'Bagus'); //.. Ini akan menghasilkan jika kita lihat di brower melalui inspect element, maka bagian judul ini akan ada atribut name yang isinya Bagus.
+
+//judul.getAttribute('id'); //..Code ini ketika di console pada browser, maka akan menghasilkan nilai id pada judul.
+
+const a = document.querySelector('section#a a');
+a.removeAttribute('href'); //..ini akan menghasilkan, href pada kata Instragram Sandhika Galih akan hilang
