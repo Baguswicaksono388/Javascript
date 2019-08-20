49e270ef77c6b069c5db9f1dc82a7a89a2f45776
Bagus Wicaksono <baguswicaksono388@gmail.com>
2019-08-20 11:41:24 +0700

    [Dev] Menghapus, Mengganti element pada Html

--- DOM/DomManipulation/domManipulation02.js
+++ DOM/DomManipulation/domManipulation02.js
@@ -22,3 +22,23 @@ const ul = document.querySelector('section#b ul'); //..ini artinya cari section 
 const li2 = document.querySelector('section#b ul li:nth-child(2)'); //..atau untuk memperkecil syntaxnya bisa juga ul.querySelector('li:nth-child(2)')
 
 ul.insertBefore(liBaru, li2);
+
+//Meghapus element a pada html kita
+const link = document.getElementsByTagName('a')[0];
+sectionA.removeChild(link);
+
+//Mengganti Paragraf 4
+const sectionB = document.querySelector('section#b'); //..kita menagkap dulu sectionnya
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const teksH2Baru = document.createTextNode('Judul Baru');
+
+h2Baru.appendChild(teksH2Baru); //..ini kita gabungkan terlebih dahulu
+
+sectionB.replaceChild(h2Baru, p4); //..ini artinya h2Baru akan mereplace p4
+
+//Skrip ini untuk memberikan warna pada element yang sudah kita edit tadi
+pBaru.style.backgroundColor = 'lightgreen';
+liBaru.style.backgroundColor = 'lightgreen';
+h2Baru.style.backgroundColor = 'lightgreen';
