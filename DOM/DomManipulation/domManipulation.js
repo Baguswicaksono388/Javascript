95e68715794ffc723229ad5eee5100d07f8f85df
Bagus Wicaksono <baguswicaksono388@gmail.com>
2019-08-20 08:22:48 +0700

    [Dev] classList add, remove, toggle, & replace

--- DOM/DomManipulation/domManipulation.js
+++ DOM/DomManipulation/domManipulation.js
@@ -14,5 +14,15 @@ judul.setAttribute('name', 'Bagus'); //.. Ini akan menghasilkan jika kita lihat 
 
 //judul.getAttribute('id'); //..Code ini ketika di console pada browser, maka akan menghasilkan nilai id pada judul.
 
-const a = document.querySelector('section#a a');
-a.removeAttribute('href'); //..ini akan menghasilkan, href pada kata Instragram Sandhika Galih akan hilang
+// const a = document.querySelector('section#a a');
+// a.removeAttribute('href'); //..ini akan menghasilkan, href pada kata Instragram Sandhika Galih akan hilang
+
+const p2 = document.querySelector('.p2'); //..ini akan memelih class p2
+p2.classList.add('label'); //..ini akan menambahkan kelas label, tepatnya pada paragref 2
+p2.classList.remove('label'); //..ini akan menghapus class label yang tadi sudah kita inputkan
+p2.classList.toggle('coba'); //..pada toogle ini lebih keren, dimana ini akan mengecek apakah class coba itu ada atau engk, klo ada maka akan hapus, jika tidak ada tambahkan.
+
+const tampilan = document.body.classList.toggle('biru-muda'); //..ini otomatis akan menambahkan class biru-muda pada body. Nah method ini bisa dikasih pada sebuah tombol, jika diklik mengaktifkan dan mematikan dari class biru muda tersebut.
+// p2.classList.item(2) //..ini tuliskan pada console pada browser, pada p2 nama class pada urutan 2 namanya apa ?
+// p2.classList.contains('coba'); //.. ini mengecek apakah ada class yang nama nya coba pada p2
+p2.classList.replace('p2', 'p2Diganti'); //..ini artinya class p2 digantikan class dengan nama p2Diganti
